efe13f3fcb80f5ff2df8137ec59f4a782eb0914a
Fabian Gabriel <121213635+gabrielf598@users.noreply.github.com>
2024-01-30 15:44:30 -0800

    Added files for the CLI

---
+++ simpleaudio_test.py
@@ -0,0 +1,22 @@
+'''
+Created for CS-370 Lab Week 3
+This is the most basic example of how to play a file using the simpleaudio package
+'''
+
+import simpleaudio as sa
+
+# This is a test sound file
+def play_sound(filename):
+    try:
+        wave_obj = sa.WaveObject.from_wave_file(filename)
+        print("I am now playing " + filename)
+        play_obj = wave_obj.play()
+        play_obj.wait_done()  # Wait until sound has finished playing
+    except:
+        print('Invalid filepath')
+
+'''
+TODO: Re-write the above as a function that takes in a filename/filepath as an argument
+and plays the sound file
+'''
+
