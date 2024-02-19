ccbc7f82a8bd591e219c0e5fed404873b93a340d
Fabian Gabriel <121213635+gabrielf598@users.noreply.github.com>
2024-02-18 20:51:56 -0800

    Added additional unit tests

--- testing.py
+++ testing.py
@@ -1,16 +1,35 @@
 import unittest
 import time 
-from simpleaudio_test import play_sound
+from simpleaudio_test import play_sound, play_sounds_at_once
 
-filename = 'sounds/coffee.wav'
+filename1 = 'sounds/coffee.wav'
+filename2 = 'sounds/toaster.wav'
 
 class testSound(unittest.TestCase):
     def test_play_time(self):
         start_time = time.time()
-        play_sound(filename)
+        play_sound(filename1)
         time.sleep(1)
         elapsed_time = time.time() - start_time
         print(elapsed_time)
+
+class testMultipleSounds(unittest.TestCase):
+    def test_play_time(self):
+        start_time = time.time()
+        play_sound([filename1,filename2])
+        time.sleep(1)
+        elapsed_time = time.time() - start_time
+        print(elapsed_time)
+    
+class testLayeredSounds(unittest.TestCase):
+    def test_play_time(self):
+        start_time = time.time()
+        play_sounds_at_once([filename1,filename2])
+        time.sleep(1)
+        elapsed_time = time.time() - start_time
+        print(elapsed_time)
+
+
 
 if __name__ == "__main__":
     unittest.main()
