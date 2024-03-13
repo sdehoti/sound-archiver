345312e39be4d563546dedaee0f368a1c22eb180
Zurain Zeeshan <zurain2001@gmail.com>
2024-03-13 15:29:39 -0700

    Added a UI element to the sound archive and also making folder functionality

---
+++ UI.py
@@ -0,0 +1,147 @@
+import os
+import sys
+import shutil
+from simpleaudio_test import play_sound, play_sounds_at_once, rename_sound_file
+
+def clear_screen():
+    if os.name == 'posix':
+        _ = os.system('clear')
+    else:
+        _ = os.system('cls')
+
+def display_menu():
+    clear_screen()
+    print("Welcome to the Sound Archive Management System\n")
+    print("1. Play one or more .wav files")
+    print("2. Play multiple .wav files simultaneously")
+    print("3. List all available files in the sounds directory")
+    print("4. Create a folder and move .wav files into it")
+    print("5. Delete a folder")
+    print("6. Remove a specific file from a folder")
+    print("7. Rename a sound folder")
+    print("8. Add files to existing folder")
+    print("9. Exit\n")
+
+def play_files(file_paths):
+    for file in file_paths:
+        play_sound(file)
+
+def play_files_simultaneously(file_paths):
+    play_sounds_at_once(file_paths)
+
+def list_available_files(folder_name):
+    print(f"Available files in the '{folder_name}' folder:")
+    directory = folder_name  # Assuming folder_name is the path to the user-created folder
+    files = os.listdir(directory)
+    for file in files:
+        print(file)  # Print each file individually
+
+    input("Press Enter to continue...")
+
+
+def create_folder(folder_name, file_paths):
+    try:
+        os.mkdir(folder_name)
+        for file_path in file_paths:
+            shutil.copy(file_path, os.path.join(folder_name, os.path.basename(file_path)))
+        print(f"Folder '{folder_name}' created successfully and files copied.")
+    except FileExistsError:
+        print(f"Folder '{folder_name}' already exists.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+def add_files_to_folder(folder_name, file_paths):
+    try:
+        for file_path in file_paths:
+            shutil.copy(file_path, os.path.join(folder_name, os.path.basename(file_path)))
+        print(f"Files duplicated into folder '{folder_name}' successfully.")
+    except FileNotFoundError:
+        print(f"Folder '{folder_name}' not found.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+
+
+def delete_folder(folder_name):
+    try:
+        shutil.rmtree(folder_name)
+        print(f"Folder '{folder_name}' deleted successfully.")
+    except FileNotFoundError:
+        print(f"Folder '{folder_name}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def remove_file_from_folder(folder_name, file_name):
+    file_path = os.path.join(folder_name, file_name)
+    try:
+        os.remove(file_path)
+        print(f"File '{file_name}' deleted successfully from folder '{folder_name}'.")
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found in folder '{folder_name}'.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def rename_sound(current_name, new_name):
+    try:
+        os.rename(current_name, new_name)
+        print(f"File '{current_name}' renamed to '{new_name}' successfully.")
+    except FileNotFoundError:
+        print(f"File '{current_name}' not found.")
+    except FileExistsError:
+        print(f"File '{new_name}' already exists.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def main():
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            print("Enter the file path(s) separated by spaces:")
+            files = input().split()
+            play_files(files)
+        elif choice == '2':
+            print("Enter the file path(s) separated by spaces:")
+            files = input().split()
+            play_files_simultaneously(files)
+        elif choice == '3':
+            folder_name = input("Enter folder name: ")
+            list_available_files(folder_name)
+        elif choice == '4':
+            print("Enter the folder name:")
+            folder_name = input()
+            print("Enter the file path(s) separated by spaces:")
+            files = input().split()
+            create_folder(folder_name, files)
+        elif choice == '5':
+            print("Enter the folder name:")
+            folder_name = input()
+            delete_folder(folder_name)
+        elif choice == '6':
+            print("Enter the folder name:")
+            folder_name = input()
+            print("Enter the file name:")
+            file_name = input()
+            remove_file_from_folder(folder_name, file_name)
+        elif choice == '7':
+            print("Enter the current folder name:")
+            current_name = input()
+            print("Enter the new folder name:")
+            new_name = input()
+            rename_sound(current_name, new_name)
+        elif choice == '8':
+            # Option 9: Add files to an existing folder
+            folder_name = input("Enter the folder name: ")
+            print("Enter the file path(s) separated by spaces:")
+            files = input().split()
+            add_files_to_folder(folder_name, files)
+        elif choice == '9':
+            print("Exiting...")
+            sys.exit(0)
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -1,97 +1,92 @@
-'''
->> python main.py --dostuff arguments
-'''
-
 import sys
 import os
-import simpleaudio as sa
+import shutil
 from simpleaudio_test import play_sound, play_sounds_at_once, rename_sound_file
+
+def create_folder(folder_name, file_paths):
+    try:
+        os.mkdir(folder_name)
+        for file_path in file_paths:
+            shutil.move(file_path, os.path.join(folder_name, os.path.basename(file_path)))
+        print(f"Folder '{folder_name}' created successfully and files moved.")
+    except FileExistsError:
+        print(f"Folder '{folder_name}' already exists.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def delete_folder(folder_name):
+    try:
+        shutil.rmtree(folder_name)
+        print(f"Folder '{folder_name}' deleted successfully.")
+    except FileNotFoundError:
+        print(f"Folder '{folder_name}' does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+def delete_file(folder_name, file_name):
+    file_path = os.path.join(folder_name, file_name)
+    try:
+        os.remove(file_path)
+        print(f"File '{file_name}' deleted successfully from folder '{folder_name}'.")
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found in folder '{folder_name}'.")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
 argvlen = len(sys.argv)
 
-'''
-This is a debugging statement that prints out the list of command line arguments that were included when you 
-invoked this program from the command line.
-'''
-print("sys.argv: ", sys.argv)
-
-'''
-# HELP: This is a command line argument that is invoked by typing --help or -h
->> python main.py --help
->> python main.py --h
-'''
-
-# This if statement checks to see what argument comes right after the program name
-# Note that it also runs if you have no arguments after the program name
-if argvlen<=1 or sys.argv[1]=='--help' or sys.argv[1]=='-h':
-
-    # This prints out a sample of how you might use this command
-    print("usage:",sys.argv[0], '--help')
+if argvlen <= 1 or sys.argv[1] == '--help' or sys.argv[1] == '-h':
+    print("usage:", sys.argv[0], '--help')
     print('-h     : print this help message and exit (also --help)')
     print('-p     : plays one or more .wav files (also --play)')
     print('-ls    : plays one or more .wav files simultaneously (also --layersounds)')
     print('-a     : lists all the available files in the sounds directory (also --all)')
-    print('-r     : renames the sound (also --rename)' )
+    print('-r     : renames the sound (also --rename)')
+    print('-cf    : create a folder and move .wav files into it')
+    print('-df    : delete a folder')
+    print('-rm    : remove a specific file from a folder')
 
-    # Note that it is proper hygiene to have this at the end of each
-    sys.exit(0);
-
-'''
-# HELP: This is another command line argument, invoked by typing --count or -c.
-For example:
->> python cli_example.py --count
->> python cli_example.py -c *
->> python3 cli_example.py -c ../sounds/*.wav
-    we use this command to count the number of wav files in sounds dir
-'''
-# Count is pretty basic: It just counts the command line arguments that come after it
-if sys.argv[1] == '-c' or sys.argv[1] == '--count' :
-    print("counted ", argvlen-2, " argument"+"s" if argvlen-2>1 else "")
     sys.exit(0)
 
-'''
-To add additional command line arguments, that have more capabilities, you need create more if statements.
-For example, if we want to play a file, we should construct a new if statement to parse the following that you might 
-enter into the command line:
->> python cli_example.py --play sounds/toaster.wav
->> python cli_example.py -p sounds/coffee.wav
+if sys.argv[1] == '-c' or sys.argv[1] == '--count':
+    print("counted ", argvlen-2, " argument"+"s" if argvlen-2 > 1 else "")
+    sys.exit(0)
 
-You can use count as a template/example.
-For now, it doesn't have to actually play a file, but it should take in a filepath as an additional argument
-that comes after play (see the examples above).
-
-For now, just have it print out the statement:
-"I am now playing <filepath>."
-
-(You'll add in the ability to actually play in the next step.)
-'''
-
-# ADD YOUR CODE FOR PLAY HERE
 if (sys.argv[1] == '-p' or sys.argv[1] == '--play') and argvlen >= 3:
     play_sound(sys.argv[2])
     sys.exit(0)
 
-#  Playing multiple sounds simultaneously 
 if (sys.argv[1] == '-ls' or sys.argv[1] == '--layersounds') and argvlen >= 3:
     play_sounds_at_once(sys.argv[2:])
     sys.exit(0)
 
-# Listing all available sounds with the command "-all"
 if (sys.argv[1] == '-a' or sys.argv[1] == '--all'):
     directory = "sounds"
     files = os.listdir(directory)
     for file in files:
-        print(file) 
+        print(file)
     sys.exit(0)
 
-# Renaming sounds 
 if (sys.argv[1] == '-r' or sys.argv[1] == '--rename'):
     rename_sound_file(sys.argv[2], sys.argv[3])
     sys.exit(0)
 
-'''
-This goes at the end of all of your if statements and it lets you know if you have 
-unknown or unaddressed command line arguments.
-'''
-print(sys.argv[0], "error, unexpected arguments ", sys.argv[1:],file=sys.stderr)
+if (sys.argv[1] == '-cf' or sys.argv[1] == '--createfolder') and argvlen >= 4:
+    folder_name = sys.argv[2]
+    file_paths = sys.argv[3:]
+    create_folder(folder_name, file_paths)
+    sys.exit(0)
 
+if (sys.argv[1] == '-df' or sys.argv[1] == '--deletefolder') and argvlen >= 3:
+    folder_name = sys.argv[2]
+    delete_folder(folder_name)
+    sys.exit(0)
+
+if (sys.argv[1] == '-rm' or sys.argv[1] == '--removefile') and argvlen >= 4:
+    folder_name = sys.argv[2]
+    file_name = sys.argv[3]
+    delete_file(folder_name, file_name)
+    sys.exit(0)
+
+print(sys.argv[0], "error, unexpected arguments ", sys.argv[1:], file=sys.stderr)
 sys.exit(1)
