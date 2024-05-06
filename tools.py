31501f7f455535fac2bcaa7ae4013327ff64bcc3
TMahlatini <mahlatit@whitman.edu>
2024-05-05 23:05:06 -0700

    added player_control  and sound frames

--- tools.py
+++ tools.py
@@ -5,37 +5,37 @@ class Tools:
     def __init__(self):
         pass
     
-    def on_play_sound(self):
+    def on_play_sound(self, object):
         pass
 
-    def on_stop_sound(self):
+    def on_stop_sound(self, object):
         pass
 
-    def on_pause_sound(self):
+    def on_pause_sound(self, object):
         pass
 
-    def on_resume_sound(self):
+    def on_resume_sound(self, object):
         pass
 
-    def on_sort_playlist(self):
+    def on_sort_playlist(self, object):
         pass
 
-    def on_edit_sound(self):
+    def on_edit_sound(self, object):
         pass
 
-    def on_record_sound(self):
+    def on_record_sound(self, object):
         pass
 
-    def on_create_playlist(self, object):
+    def on_create_playlist(self, playlist):
 
         dialog = customtkinter.CTkInputDialog(text="Enter the name of the playlist: ", title="Create Playlist")
         playlist_name = dialog.get_input()
 
         if playlist_name:
-            button = customtkinter.CTkButton(master=object.playlist_frame, 
+            button = customtkinter.CTkButton(master=playlist.playlist_frame, 
                                             text=playlist_name,
-                                            command=object.update_sounds(playlist_name))
-            button.grid(row=object.playlist_frame.grid_size()[1], column=0, padx=5, pady=5)
+                                            command=playlist.update_sounds(playlist_name))
+            button.grid(row=playlist.playlist_frame.grid_size()[1], column=0, padx=5, pady=5)
 
 
         
