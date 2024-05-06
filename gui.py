8fe9674cbee96553db760838ffea2ac153ff206e
TMahlatini <mahlatit@whitman.edu>
2024-05-05 21:01:28 -0700

    added tools.py and several tool buttons

--- gui.py
+++ gui.py
@@ -1,11 +1,12 @@
 import customtkinter
 import tkinter
 import tkinter.messagebox
+from tools import Tools
 
 customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 
-
+gt = Tools()
 
 class SoundPlayer(customtkinter.CTk):
     def __init__(self):
@@ -41,16 +42,31 @@ class SoundPlayer(customtkinter.CTk):
 
 
         self.create_playlist_button()
-        # self.sort_playlist_button()
-        # self.edit_sound_button()
-        # self.record_sound_button()
+        self.sort_playlist_button()
+        self.edit_sound_button()
+        self.record_sound_button()
 
     def create_playlist_button(self):
-        self.create_playlist_button = customtkinter.CTkButton(master=self.features_frame, text="Create Playlist", command=self.on_create_playlist)
+        self.create_playlist_button = customtkinter.CTkButton(master=self.features_frame, text="Create Playlist", command=gt.on_create_playlist)
         self.create_playlist_button.grid(row=1, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
     
+    def sort_playlist_button(self):
+        self.sort_playlist_button = customtkinter.CTkButton(master=self.features_frame, text="Sort Playlist", command=gt.on_sort_playlist)
+        self.sort_playlist_button.grid(row=2, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
 
-        
+    def edit_sound_button(self):
+        self.edit_sound_button = customtkinter.CTkButton(master=self.features_frame, text="Edit Sound File", command=gt.on_edit_sound)
+        self.edit_sound_button.grid(row=3, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
+
+    def record_sound_button(self):
+        self.record_sound_button = customtkinter.CTkButton(master=self.features_frame, text="Record Sound", command=gt.on_record_sound)
+        self.record_sound_button.grid(row=4, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
+
+    def delete_playlist_button(self):
+        self.delete_playlist_button = customtkinter.CTkButton(master=self.features_frame, text="Delete Playlist", command=gt.on_delete_playlist)
+        self.delete_playlist_button.grid(row=5, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
+
+     
     def playlists(self):
         self.playlist_frame = customtkinter.CTkScrollableFrame(self, label_text="Playlists")
         self.playlist_frame.grid(row=0, column=0, padx=(5, 5), pady=(5, 5), sticky="nsew")
@@ -63,8 +79,9 @@ class SoundPlayer(customtkinter.CTk):
             button.grid(row=i, column=0, padx=10, pady=10) # Adjust padx and pady as needed
 
 
-    def on_create_playlist(self):
-        tkinter.messagebox.showinfo("Create Playlist", "Create Playlist button clicked")
+    
+
+   
 
     def update_sounds(self, playlist):
         pass
